ac37c23ce3186c46345c29b593d4f2f070bf4016
Viviana Rosenbaum <vivianarosenbaum@gmail.com>
2024-08-12 23:08:51 -0600

    Reverted back to previous working state

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://vhkyoconiayqwtdxickg.supabase.co';
-const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoa3lvY29uaWF5cXd0ZHhpY2tnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI5OTQzNzUsImV4cCI6MjAzODU3MDM3NX0.erVdi1gxUEiVDqz1ykkR8KWZOzUQQmLOglnNCn12mwo';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
